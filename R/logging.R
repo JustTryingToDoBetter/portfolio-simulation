77441fff0a729a29393d1107068d9e0e14ed948c
Jaydin <103458151+JustTryingToDoBetter@users.noreply.github.com>
2026-03-04 11:43:23 +0000

    Refactor logging functions to use a centralized log_message function for improved structure and consistency

--- R/logging.R
+++ R/logging.R
@@ -1,11 +1,18 @@
 # Intent: minimal structured logging for CLI runs
 
-log_info <- function(..., .sep= ""){
+log_message <- function(level, ..., .sep = "") {
     msg <- paste0(...)
-    cat(sprintf("[%s] INFO %s\n", format(Sys.time(), "%Y-%m-%d %H:%M:%S"), msg), sep = .sep)
+    cat(sprintf("[%s] %s %s\n", format(Sys.time(), "%Y-%m-%d %H:%M:%S"), level, msg), sep = .sep)
 }
 
-log_warn <- function(..., .sep = ""){
-    msg <- paste0(...)
-    cat(sprintf("[%s] WARN %s\n", format(Sys.time(), "%Y-%m-%d %H:%M:%S"), msg), sep = .sep)
+log_info <- function(..., .sep = "") {
+    log_message("INFO", ..., .sep = .sep)
+}
+
+log_warn <- function(..., .sep = "") {
+    log_message("WARN", ..., .sep = .sep)
+}
+
+log_error <- function(..., .sep = "") {
+    log_message("ERROR", ..., .sep = .sep)
 }
